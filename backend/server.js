397c8b297c4282d103ad914138dce96141ff478c
Chinwike <maduabuchichizzy@gmail.com>
2022-06-16 11:35:08 +0100

    feat(backend): add settings for production deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -17,6 +18,17 @@ app.use(express.json())
 
 // API routes
 app.use('/api/user', userRoutes)
+
+// deployment configuration
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+}
 
 // Middleware
 app.use(notFound)
