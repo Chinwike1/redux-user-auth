0fa22e852e5a338eebf99668405eaae7af09b2fb
Chinwike <maduabuchichizzy@gmail.com>
2022-06-16 11:13:35 +0100

    fix(frontend): remove redirect rule in profile screen

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,19 +1,8 @@
-import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
 import '../styles/profile.css'
 
 const ProfileScreen = () => {
   const { userInfo } = useSelector((state) => state.user)
-
-  const navigate = useNavigate()
-
-  // redirect unauthenticated user to login screen
-  useEffect(() => {
-    if (userInfo === null) {
-      navigate('/')
-    }
-  }, [userInfo, navigate])
 
   return (
     <div>
